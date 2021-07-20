1d22c1fbb54feb4e56cf26c9e0712a9c4812b804
Ryan Searls <Ryan.searls@gmail.com>
2021-07-20 13:49:11 -0500

    added create product and showproduct methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
     <h1>New Recipe</h1>
+    <div>
+      Name:
+      <input type="text" v-model="newProductParams.name" />
+      Price:
+      <input type="text" v-model="newProductParams.price" />
+      Description:
+      <input type="text" v-model="newProductParams.description" />
+      Image_url:
+      <input type="text" v-model="newProductParams.image_url" />
+    </div>
     <button v-on:click="createProduct()">Create</button>
     <h1>{{ message }}</h1>
     <p>{{ message1 }}</p>
     <!-- <button v-on:click="click">hello</button> -->
     <div v-for="product in products" v-bind:key="product.id">
-      <h1>{{ product.name }} </h1>
+      <h1>{{ product.name }}</h1>
       <h2>{{ product.price }}</h2>
-      <img v-bind:src="product.image_url" :alt="product.name" />
+      <img v-bind:src="product.image_url" v-bind:alt="product.name" />
+      <button v-on:click="showProduct(product)">More Info!</button>
     </div>
+    <dialog id="product-details">
+      <form method="dialog">
+        <h1>Product Info</h1>
+        <p>Name: {{ currentProduct.name }}</p>
+        <p>Price:{{ currentProduct.price }}</p>
+        <p>Description:{{ currentProduct.description }}</p>
+        <p>Image_url:{{ currentProduct.image_url }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
+
 <style></style>
+
 <script>
 import axios from "axios";
 
@@ -21,7 +44,9 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       message1: "How are you?",
-      products: [],
+      newProductParams: {},
+      products: {},
+      currentProduct: {},
     };
   },
   created: function () {
@@ -33,23 +58,21 @@ export default {
         this.products = response.data;
         console.log("All Products:", this.products);
       });
-    },  
+    },
     createProduct: function () {
       console.log("yooooo");
-  
-      var params = {
-        name: "Fish",
-        price: 5,
-        description: "Small and weak",
-        image_url: "image.png",
-      };
-      axios
-        .post("http://localhost:3000/recipes", params)
+       axios
+        .post("http://localhost:3000/products", this.newProductParams)
         .then((response) => {
           console.log("Success!", response.data);
-          this.recipes.push(response.data);
+          this.products.push(response.data);
         })
         .catch((error) => console.log(error.response));
+      },
+      showProduct: function (product) {
+      console.log(product);
+      this.currentProduct = product;
+      document.querySelector("#product-details").showModal();
     },
   },
 };
