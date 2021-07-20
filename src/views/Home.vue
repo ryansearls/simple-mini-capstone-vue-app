358bb0ca04b67478768e82e70cb36336defedb19
Ryan Searls <Ryan.searls@gmail.com>
2021-07-20 10:52:49 -0500

    added create function to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>New Recipe</h1>
+    <button v-on:click="createProduct()">Create</button>
+    <h1>{{ message }}</h1>
+    <p>{{ message1 }}</p>
+    <!-- <button v-on:click="click">hello</button> -->
+    <div v-for="product in products" v-bind:key="product.id">
+      <h1>{{ product.name }} </h1>
+      <h2>{{ product.price }}</h2>
+      <img v-bind:src="product.image_url" :alt="product.name" />
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      message1: "How are you?",
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/products").then((response) => {
+        this.products = response.data;
+        console.log("All Products:", this.products);
+      });
+    },  
+    createProduct: function () {
+      console.log("yooooo");
+  
+      var params = {
+        name: "Fish",
+        price: 5,
+        description: "Small and weak",
+        image_url: "image.png",
+      };
+      axios
+        .post("http://localhost:3000/recipes", params)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.recipes.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
   },
 };
 </script>
